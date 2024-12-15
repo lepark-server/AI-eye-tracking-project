cb3343391b429c7340736c9a09b8893e3f29876b
Lok <ngoquangloc200501@gmail.com>
2024-12-15 21:36:07 +0700

    Add face and eye tracking

---
+++ build/CMakeFiles/FaceMeshCpp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/DetectionPostProcess.cpp" "CMakeFiles/FaceMeshCpp.dir/src/DetectionPostProcess.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/DetectionPostProcess.cpp.o.d"
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/FaceDetection.cpp" "CMakeFiles/FaceMeshCpp.dir/src/FaceDetection.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/FaceDetection.cpp.o.d"
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/FaceLandmark.cpp" "CMakeFiles/FaceMeshCpp.dir/src/FaceLandmark.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/FaceLandmark.cpp.o.d"
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/IrisLandmark.cpp" "CMakeFiles/FaceMeshCpp.dir/src/IrisLandmark.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/IrisLandmark.cpp.o.d"
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/ModelLoader.cpp" "CMakeFiles/FaceMeshCpp.dir/src/ModelLoader.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/ModelLoader.cpp.o.d"
+  "/home/lok/Work/Morning_Project/AI/mediapipe_face_iris_cpp/src/demo.cpp" "CMakeFiles/FaceMeshCpp.dir/src/demo.cpp.o" "gcc" "CMakeFiles/FaceMeshCpp.dir/src/demo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FaceMeshCpp.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceMeshCpp.dir/src/DetectionPostProcess.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/DetectionPostProcess.cpp.o.d"
+  "CMakeFiles/FaceMeshCpp.dir/src/FaceDetection.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/FaceDetection.cpp.o.d"
+  "CMakeFiles/FaceMeshCpp.dir/src/FaceLandmark.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/FaceLandmark.cpp.o.d"
+  "CMakeFiles/FaceMeshCpp.dir/src/IrisLandmark.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/IrisLandmark.cpp.o.d"
+  "CMakeFiles/FaceMeshCpp.dir/src/ModelLoader.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/ModelLoader.cpp.o.d"
+  "CMakeFiles/FaceMeshCpp.dir/src/demo.cpp.o"
+  "CMakeFiles/FaceMeshCpp.dir/src/demo.cpp.o.d"
+  "FaceMeshCpp"
+  "FaceMeshCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceMeshCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
